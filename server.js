dcc10918756795c94321d6e51500be0948b2909c
Darren Anastasoff <soleman16@gmail.com>
2018-12-20 16:40:09 -0500

    added a picture to the home screen

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('views/images')); 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
